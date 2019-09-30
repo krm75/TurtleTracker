1facb15811c6a619c57242fd955dc767a90fb01b
Kim Myers <krm75@duke.edu>
2019-09-30 12:10:55 -0400

    Create AGROSTracker.py

---
+++ AGROSTracker.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTracker.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Kim Myers (krm75@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
+
+
+
